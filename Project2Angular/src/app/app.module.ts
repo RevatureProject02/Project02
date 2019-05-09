2cb1e4549877ff4ca080e99ea8b359706739b35e
Rico <RicoMRichardson@gmail.com>
2019-05-09 13:13:47 -0400

    navbar addition

--- Project2Angular/src/app/app.module.ts
+++ Project2Angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 //components:
 import { AppComponent } from './app.component';
 import { SGradesComponent } from './components/sgrades/sgrades.component';
+import { StudentNavbarComponent } from './components/student-navbar/student-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SGradesComponent
+    SGradesComponent,
+    StudentNavbarComponent
   ],
   imports: [
     BrowserModule,
