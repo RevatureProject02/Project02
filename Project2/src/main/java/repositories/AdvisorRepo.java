c259177d86ed6f6c57132761c1d7072e04fbc760
Candace Stern <stern.candace@gmail.com>
2019-05-10 15:51:03 -0400

    candace update

--- Project2/src/main/java/repositories/AdvisorRepo.java
+++ Project2/src/main/java/repositories/AdvisorRepo.java
@@ -1,5 +1,41 @@
 package repositories;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+
+import model.Advisor;
+
+@Transactional
+@EnableTransactionManagement
+@Repository
 public class AdvisorRepo {
+	
+	@Autowired
+	SessionFactory sf;
+	
+	public String insertAdvisor(Advisor a) {
+		Session s= sf.getCurrentSession();
+		s.persist(a);
+		return "added";
+	}
+	
+	public List<Advisor> selectAllAdvisors(){
+		List<Advisor> advisors = new ArrayList<Advisor>();
+		Session s = sf.getCurrentSession();
+		Criteria cr = s.createCriteria(Advisor.class);
+		advisors = cr.list();
+		return advisors;
+	}
+	
+	
+	
 
 }
