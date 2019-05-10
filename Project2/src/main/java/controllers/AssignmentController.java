efdb386488c4b39c7b24a0cbea7259c1414a5df9
Rico <RicoMRichardson@gmail.com>
2019-05-10 16:54:38 -0400

    reading pull

--- Project2/src/main/java/controllers/AssignmentController.java
+++ Project2/src/main/java/controllers/AssignmentController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import model.Assignment;
-import model.Student;
 import services.AssignmentServices;
 
 @Controller
